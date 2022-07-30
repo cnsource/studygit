87cfed598b4c18e9c596c3e09c1f0f526d7d4afb
cnsource <2274850295@qq.com>
2022-07-30 23:20:41 +0800

    feat: 增加弹窗

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,52 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from "vue";
 
-const isOpen = ref(false)
+const isOpen = ref(false);
 
-function changeModetoDark(){
+function changeModetoDark() {
   let root = document.getElementById("root");
-  root.classList.add('dark')
+  root.classList.add("dark");
 }
 
-function changeModetoLight(){
+function changeModetoLight() {
   let root = document.getElementById("root");
-  root.classList.remove('dark')
+  root.classList.remove("dark");
 }
 
-function showName(){
-  alert('你好小刘同学')
+function showName() {
+  alert("你好小刘同学");
 }
 </script>
 
 <template>
-    <div class="w-full h-screen bg-sky-400 dark:bg-slate-300 flex justify-center items-center">
-        <button @click="showName" class="px-4 py-1.5 border-white border-2 text-white">点击输出你好小刘同学</button>
-        <button @click="changeModetoDark" class="px-4 py-1.5 border-white border-2 text-white">切换暗模式</button>
-        <button @click="changeModetoLight" class="px-4 py-1.5 border-white border-2 text-white">切换亮模式</button>
-    </div>
+  <div
+    class="w-full h-screen bg-sky-400 dark:bg-slate-300 flex justify-center items-center"
+  > 
+    <dialog class="w-16 h-16" :open="isOpen">
+      hello
+    </dialog>
+    <button
+      @click="showName"
+      class="px-4 py-1.5 border-white border-2 text-white"
+    >
+      点击输出你好小刘同学
+    </button>
+    <button
+      @click="changeModetoDark"
+      class="px-4 py-1.5 border-white border-2 text-white"
+    >
+      切换暗模式
+    </button>
+    <button
+      @click="changeModetoLight"
+      class="px-4 py-1.5 border-white border-2 text-white"
+    >
+      切换亮模式
+    </button>
+    <button @click="isOpen = !isOpen">
+      openDialog
+    </button>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
