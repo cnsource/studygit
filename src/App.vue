183913691a3e81c080b9fbb104e9d5d0126877a8
cnsource <2274850295@qq.com>
2022-07-30 22:52:14 +0800

    init: 初始化工程

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue';
+
+const isOpen = ref(false)
+
+function changeModetoDark(){
+  let root = document.getElementById("root");
+  root.classList.add('dark')
+}
+
+function changeModetoLight(){
+  let root = document.getElementById("root");
+  root.classList.remove('dark')
+}
+</script>
+
+<template>
+    <div class="w-full h-screen bg-sky-400 dark:bg-slate-300 flex justify-center items-center">
+        <button @click="changeModetoDark" class="px-4 py-1.5 border-white border-2 text-white">切换暗模式</button>
+        <button @click="changeModetoLight" class="px-4 py-1.5 border-white border-2 text-white">切换亮模式</button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
