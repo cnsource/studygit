99c5301d20e73d901d24e01af5a4bdf8cd8a6d4e
刘心怡 <1348298699@qq.com>
2022-07-30 23:04:40 +0800

    修改了App.vue

--- src/App.vue
+++ src/App.vue
@@ -12,10 +12,15 @@ function changeModetoLight(){
   let root = document.getElementById("root");
   root.classList.remove('dark')
 }
+
+function showName(){
+  alert('你好小刘同学')
+}
 </script>
 
 <template>
     <div class="w-full h-screen bg-sky-400 dark:bg-slate-300 flex justify-center items-center">
+        <button @click="showName" class="px-4 py-1.5 border-white border-2 text-white">点击输出你好小刘同学</button>
         <button @click="changeModetoDark" class="px-4 py-1.5 border-white border-2 text-white">切换暗模式</button>
         <button @click="changeModetoLight" class="px-4 py-1.5 border-white border-2 text-white">切换亮模式</button>
     </div>
